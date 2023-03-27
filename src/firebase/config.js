695bbd1a9f0697ddff704e2ff755e5d3c7845a68
Okpugie Kindness <64986593+Owieoghene16@users.noreply.github.com>
2023-03-27 16:41:18 +0100

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,11 +1,11 @@
 const Config = {
-  apiKey: 'AIzaSyDrytGQ4oSC_h9D87T-6sANo48p7ZdCJm8',
-  authDomain: 'kindness-chatbox-bc775.firebaseapp.com',
-  projectId: 'kindness-chatbox-bc775',
-  storageBucket: 'kindness-chatbox-bc775.appspot.com',
-  messagingSenderId: '1032385109983',
-  appId: '1:1032385109983:web:7ab1645f0608ab6528ae6a',
-  measurementId: 'G-MTSDZ8MMSZ'
+  apiKey: 'Your apiKey',
+  authDomain: 'your authDomain',
+  projectId: 'your projectId',
+  storageBucket: 'your storageBucket',
+  messagingSenderId: 'your messagingSenderId',
+  appId: 'your appId',
+  measurementId: 'your measurementId'
 };
 
 export default Config;
